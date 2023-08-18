cd7a70a6a40f66423e8e1fe77d51aaf35e00fda5
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2023-08-18 12:12:47 +0100

    Done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,13 +7,17 @@ const Cart = ({showCart}) => {
   const cartContext = useContext(CartContext)
 
   return (
-    <div className={`${ showCart ? "" : "w-0 px-0"} flex flex-col transition-all delay-150 duration-300 overflow-hidden bg-gradient-to-r from-lime-600 from-40% to-emerald-600 shadow-stone-600 shadow-lg rounded-md w-1/4 px-10 py-8`}>
+    <div className={`${ showCart ? "w-4/12 px-10" : "w-0 px-0"} sticky top-28 h-fit min-h-[280px] flex flex-col transition-all delay-150 duration-300 overflow-hidden bg-gradient-to-r from-lime-600 from-40% to-emerald-600 shadow-stone-600 shadow-lg rounded-md py-8`}>
       <h3 className="flex gap-3 font-bold text-stone-100 drop-shadow-sm mb-8">
         <ShoppingCartIcon className='w-5'/>
         CHECKOUT
       </h3>
 
       <div className="flex flex-col gap-4">
+        {cartContext.cart.length === 0 && 
+          <p>No items in your cart</p>
+        }
+
         {cartContext.cart.map((item) => {
           return <CartItem key={item.dish.name} item={item} />
         })}
@@ -23,9 +27,9 @@ const Cart = ({showCart}) => {
       <hr />
       <br />
 
-      <div className="flex flex-grow justify-between">
+      <div className="flex flex-grow justify-between mb-5">
         <p className="font-bold text-stone-900">TOTAL</p>
-        <p className="font-bold text-stone-900">$ {cartContext.cart.length > 0 ? cartContext.cart.reduce((total, item) => total + item.quantity * item.dish.price, 0) : 0}</p>
+        <p className="font-bold text-stone-900">$ {cartContext.cart.length > 0 ? cartContext.cart.reduce((total, item) => total + item.quantity * item.dish.price, 0).toFixed(2) : 0}</p>
       </div>
 
       <div className="flex gap-3 justify-end">
--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -1,15 +1,27 @@
 import { ShoppingCartIcon } from '@heroicons/react/24/solid'
 import "./Navbar.css"
-import { useContext } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import CartContext from '../../../contexts/cart-context'
 
 const Navbar = ({showCartHandler}) => {
+  const cartLength = useRef()
   const cartContext = useContext(CartContext)
+
+  useEffect(() => {
+    if (cartContext.cart.length > 0) {
+      cartLength.current.classList.add("increase-cart")
+    }
+  }, [cartContext.cart])
+
+  const buttonClickHandler = () => {
+    cartLength.current.classList.toggle("border-lime-100")
+    showCartHandler()
+  }
 
   return (
     <div className="bg-invert z-10 px-10 py-5 flex justify-between bg-gradient-to-r from-lime-500/90 to-lime-400/30 fixed top-0 right-0 left-0">
       <h1 className="text-lime-50 text-2xl drop-shadow-lg flex justify-center items-center">ReactMeals</h1>
-      <button className='flex text-lime-50 bg-gradient-to-r from-lime-600 from-40% to-emerald-600 rounded-xl px-3 py-2 text-sm gap-2 border-2 border-lime-600 hover:shadow-none hover:border-2 hover:border-lime-100 items-center' onClick={showCartHandler}>
+      <button key={Math.random() * 1000} ref={cartLength} className='flex text-lime-50 bg-gradient-to-r from-lime-600 from-40% to-emerald-600 rounded-xl px-3 py-2 text-sm gap-2 border-2 border-lime-600 hover:shadow-none hover:border-2 hover:border-lime-100 items-center' onClick={buttonClickHandler}>
         <ShoppingCartIcon className='w-5'/>
         <p>Checkout</p>
         <p className='rounded-full bg-stone-100 px-3 py-0.5 flex justify-center text-lime-700 items-center font-bold'>{cartContext.cart.length}</p>
